a053ccf4810c9e034ab8c639e24199d278d7505a
toneyparky <qkrtmddhks95@gmail.com>
2021-02-26 22:56:36 +0900

    feat(discovery_go): 7장 1번 문제
    - go routine 퀵소트 구현

--- discovery_go/chap_7/question_1.go
+++ discovery_go/chap_7/question_1.go
@@ -27,6 +27,37 @@ func partition(s []int, low, high int) int {
 	return i + 1
 }
 
+func GoRoutineQuickSort(s []int, low, high int) {
+	d := make(chan int)
+	go goRoutineQuickSort(s, low, high, d)
+	<-d
+	return
+}
+
+func goRoutineQuickSort(s []int, low, high int, done chan int) {
+	if low >= high {
+		done <- 1
+		return
+	}
+	if len(s) <= 1 {
+		done <- 1
+		return
+	}
+
+	pivotIdx := partition(s, low, high)
+
+	childChan := make(chan int)
+	go goRoutineQuickSort(s, low, pivotIdx-1, childChan)
+	go goRoutineQuickSort(s, pivotIdx+1, high, childChan)
+
+	for i := 0; i < 2; i++ {
+		<-childChan
+	}
+
+	done <- 1
+	return
+}
+
 func main() {
 	ints := []int{1, 2, 45, 61, 7, 3, 5, 6, 8}
 	Quicksort(ints, 0, len(ints)-1)
@@ -34,4 +65,13 @@ func main() {
 	for _, each := range ints {
 		fmt.Println(each)
 	}
+
+	fmt.Println("======================")
+
+	ints2 := []int{1, 2, 45, 61, 7, 3, 5, 6, 8}
+	GoRoutineQuickSort(ints2, 0, len(ints2)-1)
+	for _, each := range ints2 {
+		fmt.Println(each)
+	}
+
 }
